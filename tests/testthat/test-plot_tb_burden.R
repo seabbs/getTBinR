96c5a498d61e5b838f5f4d27a30a962d5d6af02b
seabbs <s.e.abbott12@gmail.com>
2017-12-27 16:27:55 +0000

    added basic tests for plot existance

---
+++ tests/testthat/test-plot_tb_burden.R
@@ -0,0 +1,15 @@
+context("plot_tb_burden")
+
+plot <- plot_tb_burden()
+
+plot_int <- plot_tb_burden(interactive = TRUE)
+
+test_that("plot_tb_burden produces a plot", {
+  expect_true(!is.null(plot))
+  expect_equal("ggplot", class(plot)[2])
+})
+
+test_that("plot_tb_burden produces an interactive plot", {
+  expect_true(!is.null(plot_int))
+  expect_equal("plotly", class(plot_int)[1])
+})
---
+++ tests/testthat/test-plot_tb_burden_overview.R
@@ -0,0 +1,15 @@
+context("plot_tb_burden_overview")
+
+plot <- plot_tb_burden_overview()
+
+plot_int <- plot_tb_burden_overview(interactive = TRUE)
+
+test_that("plot_tb_burden_overview produces a plot", {
+  expect_true(!is.null(plot))
+  expect_equal("ggplot", class(plot)[2])
+})
+
+test_that("plot_tb_burden_overview produces an interactive plot", {
+  expect_true(!is.null(plot_int))
+  expect_equal("plotly", class(plot_int)[1])
+})
