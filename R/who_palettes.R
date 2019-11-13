3e98863ee597de864de80de929ff6aa248c54dce
Maria <33788128+mariabnd@users.noreply.github.com>
2019-11-13 18:59:48 +0100

    Add green to main palette and colours from figs 3.4-3.5, 3.17, 3.26, and 3.29-3.31

--- R/who_palettes.R
+++ R/who_palettes.R
@@ -15,8 +15,22 @@
 #' who_palettes(palette = "main", reverse = TRUE)
 who_palettes <- function(palette = "main", reverse = FALSE, ...){
   
-  who_pal <- list("main" = c("#000000", "#EE1A24", "#1591D1"),
-                  "light" = c("#000000", "#FCBEA7", "#BDD5EF"))
+  who_pal <- list("main" = c("#000000", "#EE1A24", "#1591D1", "#90A93D"),
+                  "light" = c("#000000", "#FCBEA7", "#BDD5EF", "#D9DDBB"),
+                  "greens" = c("#EEEDD9", "#DCDCB0", "#C7C981", "#AFB547",
+                               "#9AA372", "#5C7124"),
+                  "purples" = c("#F0DFE5", "#E6C3CF", "#B59BC9", "#946CAF",
+                                "#6F2B90"),
+                  "blues" = c("#E2F3F4", "#AEDFE6", "#76CDD9", "#00B7C7",
+                              "#00828F"),
+                  "pinks" = c("#F2E4E8", "#E6C3CF", "#D79AB0", "#C76B90",
+                              "#B62170"),
+                  "greens2" = c("#EAEBD9", "#D9DDBB", "#D9DDBB", "#C4CC94",
+                                "#7FCAAD", "#3DBA93", "#00A66C"),
+                  "browns" = c("#F3DED1", "#EBC4AC", "#E1A280", "#B7988F",
+                               "#A07A71", "#82524C"),
+                  "blues2" = c("#E0E2F1", "#BAC0E1", "#98A1D1", "#5C72B6",
+                               "#034DA1"))
   
   pal <- who_pal[[palette]]
   if (reverse) pal <- rev(pal)
