abfedfaee7edf299cd80c709f28421651f08debd
Maria <33788128+mariabnd@users.noreply.github.com>
2019-12-04 21:06:38 +0100

    Add misc and green as well as more colours for main and light
    Other options found in the report used for green were
    #BEE2D2, #76CFAD, #00A66C
    and
    #EDECD7, #DCDCB0, #D1D387, #B4BA3E, #9AA372, #5C7124

--- R/who_palettes.R
+++ R/who_palettes.R
@@ -23,14 +23,14 @@ who_palettes <- function(palette = "main", reverse = FALSE, n = NULL,
                          add_missings = FALSE,...){
   
   if (!palette %in% c("main", "light", "purple", "turquoise",
-                      "blue", "magenta", "brown"))
+                      "blue", "magenta", "brown", "misc", "green"))
     stop("chosen palette not available")
   
   if (palette == "main")
-    who_pal <- list(c("#000000", "#EE1A24", "#1591D1", "#90A93D"))
+    who_pal <- list(c("#000000", "#EE1A24", "#1591D1", "#90A93D", "#00A56B", "#008ECF"))
   
   if (palette == "light")
-    who_pal <- list(c("#000000", "#FCBEA7", "#BDD5EF", "#D9DDBB"))
+    who_pal <- list(c("#BBBDBF", "#FCBEA7", "#BDD5EF", "#D9DDBB", "#99D3BC", "#BCD4EE"))
   
   if (palette == "purple")
     who_pal <- list(c("#6F2B90"),
@@ -60,16 +60,29 @@ who_palettes <- function(palette = "main", reverse = FALSE, n = NULL,
                     c("#FEE5CA", "#FDCA93", "#DC936D", "#CB572C", "#82524C"), 
                     c("#F3DED1", "#EBC4AC", "#E1A280", "#B7988F", "#A07A71", "#82524C"))
   
+  if (palette == "misc")
+    who_pal <- list(c("#00AABC"),
+                    c("#00AABC", "#ED1C24"),
+                    c("#00AABC", "#ED1C24", "#F7931D"),
+                    c("#00AABC", "#ED1C24", "#F7931D", "#E5DAB8"))
+  
+  if (palette == "green")
+    who_pal <- list(c("#CFE4AD"),
+                    c("#90A93D", "#CED4A7"),
+                    c("#EAEBD9", "#CED4A7", "#AEBC70"),
+                    c("#DEF0E7", "#A6D8C3", "#62C29F", "#00AE70"),
+                    c("#EAEBD9", "#D7DBB7", "#C4CC94", "#7FCAAD", "#3FC39A", "#00A66C"))
+  
   len <- sapply(1 : length(who_pal), function(x) length(who_pal[[x]]))
   if (is.null(n)) {
     pal <- who_pal[[which.max(len)]]
   } else {
-  if (n > which.max(len))
-    stop("n is larger than number of available colours for chosen palette")
-  if (n %in% len)
-    pal <- who_pal[[which(len == n)]]
+    if (n > which.max(len))
+      stop("n is larger than number of available colours for chosen palette")
+    if (n %in% len)
+      pal <- who_pal[[which(len == n)]]
   }
-    
+  
   if (isTRUE(reverse))
     pal <- rev(pal)
   
