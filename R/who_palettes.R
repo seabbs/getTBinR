5f096cf512b34fe2dce8d5ef094035d90f9486b8
Maria <33788128+mariabnd@users.noreply.github.com>
2019-12-04 20:55:00 +0100

    Suggested new structure of who_palette
    Other options for blues found in the TB report were
    c("#CFF5FF", "#A8C8E9", "#53A0D8", "#034DA1") for length 4 and
    c("#E2F3F4", "#AEDFE6", "#76CDD9", "#00B7C7", "#00828F") for length 5
    but these did not seem to have the same amount of overlap with the other options as those used

--- R/who_palettes.R
+++ R/who_palettes.R
@@ -3,7 +3,11 @@
 #' @param palette A character string. the colours of choice, current options are "main" and "light"
 #' for mixed colours as well as "purples", "blues", "pinks", "greens", and "browns".
 #' @param reverse Logical, defaults to \code{FALSE}. Should the palette be reversed.
-#' @param n Number of colours desired, defaults to NULL which returns the entire palette.
+#' @param n Number of colours desired. If a specific value is given which corresponds 
+#' to the length of a palette used in the 2019 WHO TB report, this palette is returned, 
+#' else the number of entries in the palette with the most colour options (up to its 
+#' full length) is returned. Defaults to \code{NULL} which returns the longest palette.
+#' @param add_missings Add a grey and white colour to the palette, defaults to \code{FALSE}.
 #' @param ... Pass additional arguments to \code{colorRampPalette}.
 #' @author Maria Bekker-Nielsen Dunbar
 #' @author Sam Abbott
@@ -15,27 +19,67 @@
 #' 
 #' # Set up the main palette but reversed.
 #' who_palettes(palette = "main", reverse = TRUE)
-who_palettes <- function(palette = "main", reverse = FALSE, n = NULL, ...){
+who_palettes <- function(palette = "main", reverse = FALSE, n = NULL, 
+                         add_missings = FALSE,...){
   
-  who_pal <- list("main" = c("#000000", "#EE1A24", "#1591D1", "#90A93D"),
-                  "light" = c("#000000", "#FCBEA7", "#BDD5EF", "#D9DDBB"),
-                  "purples" = c("#F0DFE5", "#E6C3CF", "#B59BC9", "#946CAF",
-                                "#6F2B90"),
-                  "blues" = c("#E2F3F4", "#AEDFE6", "#76CDD9", "#00B7C7",
-                              "#00828F"),
-                  "pinks" = c("#F2E4E8", "#E6C3CF", "#D79AB0", "#C76B90",
-                              "#B62170"),
-                  "greens" = c("#EAEBD9", "#D9DDBB", "#D9DDBB", "#C4CC94",
-                                "#7FCAAD", "#3DBA93", "#00A66C"),
-                  "browns" = c("#F3DED1", "#EBC4AC", "#E1A280", "#B7988F",
-                               "#A07A71", "#82524C"))
+  if (!palette %in% c("main", "light", "purple", "turquoise",
+                      "blue", "magenta", "brown"))
+    stop("chosen palette not available")
   
-  pal <- who_pal[[palette]]
-  if (n > length(pal))
+  if (palette == "main")
+    who_pal <- list(c("#000000", "#EE1A24", "#1591D1", "#90A93D"))
+  
+  if (palette == "light")
+    who_pal <- list(c("#000000", "#FCBEA7", "#BDD5EF", "#D9DDBB"))
+  
+  if (palette == "purple")
+    who_pal <- list(c("#6F2B90"),
+                    c("#A787BE", "#6F2B90"),
+                    c("#E7E1EF", "#C5B1D6", "#9A75B4", "#6F2B90"),
+                    c("#F0DFE5", "#E6C3CF", "#B59BC9", "#946CAF", "#6F2B90"),
+                    c("#F2E4E8", "#EACED7", "#DAA3B7", "#B59BC9", "#A07EB9", "#6E4B84"))
+  
+  if (palette == "turquoise")
+    who_pal <- list(c("#D1E9EC", "#9AD2DA", "#3AB6C3", "#009CAC"),
+                    c("#E2F3F4", "#AEDFE6", "#76CDD9", "#00B7C7", "#00828F"))
+  
+  if (palette == "blue")
+    who_pal <- list(c("#034DA1"),
+                    c("#E0E2F1", "#8490C7", "#034DA1"),
+                    c("#E0E2F1", "#AEB5DC", "#7080BE", "#034DA1"),
+                    c("#E0E2F1", "#98A1D1", "#BBC1E2", "#98A1D1", "#034DA1"))
+  
+  if (palette == "magenta")
+    who_pal <- list(c("#B62170"),
+                    c("#F0DFE5", "#E2B8C7", "#CD7D9C", "#B62170"),
+                    c("#F2E4E8", "#E6C3CF", "#D79AB0", "#C76B90", "#B62170"))
+  
+  if (palette == "brown")
+    who_pal <- list(c("#82524C"),
+                    c("#F7931D", "#82524C"), 
+                    c("#FEE5CA", "#FDCA93", "#DC936D", "#CB572C", "#82524C"), 
+                    c("#F3DED1", "#EBC4AC", "#E1A280", "#B7988F", "#A07A71", "#82524C"))
+  
+  len <- sapply(1 : length(who_pal), function(x) length(who_pal[[x]]))
+  if (is.null(n)) {
+    pal <- who_pal[[which.max(len)]]
+  } else {
+  if (n > which.max(len))
     stop("n is larger than number of available colours for chosen palette")
-  if(isFALSE(is.null(n)))
-    pal <- pal[1 : n]
-  if (reverse)
+  if (n %in% len)
+    pal <- who_pal[[which(len == n)]]
+  }
+    
+  if (isTRUE(reverse))
     pal <- rev(pal)
+  
+  if (isTRUE(add_missings)) {
+    if (palette == "light") {
+      #pal <- c(pal, c())
+    } else {
+      pal <- c(pal, c("#9D9B9C", "#FFFFFF"))
+    }
+  }
+  
   grDevices::colorRampPalette(pal, ...)
 }
