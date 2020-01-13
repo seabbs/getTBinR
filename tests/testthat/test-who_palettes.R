544267aaf02bcd08c178e2b9e3ecbbb04f25bdcf
Maria <33788128+mariabnd@users.noreply.github.com>
2020-01-13 22:02:06 +0100

    Removed 4

--- tests/testthat/test-who_palettes.R
+++ tests/testthat/test-who_palettes.R
@@ -3,7 +3,7 @@ context("who_palettes")
 test_that("who_palettes cannot take n larger than palettes or theme not provided", {
   
   large_n <- who_palettes(palette = "main", n = 100)
-  expect_error(large_n, "n is larger than number of available colours for chosen palette")4
+  expect_error(large_n, "n is larger than number of available colours for chosen palette")
   
   wrong_col <- who_palettes(palette = "dark")
   expect_error(wrong_col, "chosen palette not available")
