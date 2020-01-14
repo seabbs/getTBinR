725e3ea9d7e0ce6e44142f5304a20041fc7366c0
Sam Abbott <contact@samabbott.co.uk>
2020-01-14 09:29:14 +0000

    Update test-who_palettes.R

--- tests/testthat/test-who_palettes.R
+++ tests/testthat/test-who_palettes.R
@@ -1,10 +1,10 @@
 context("who_palettes")
 
-test_that("who_palettes cannot take n larger than palettes or theme not provided", {
-  
-  large_n <- who_palettes(palette = "main", n = 100)
-  expect_error(large_n, "n is larger than number of available colours for chosen palette")
-  
-  wrong_col <- who_palettes(palette = "dark")
-  expect_error(wrong_col, "chosen palette not available")
+test_that("who_palettes cannot take n larger than palettes", {
+  expect_error(who_palettes(palette = "main", n = 100))
+  }) 
+
+
+test_that("who_palettes fails if a unrecognised palette is supplied", {
+  expect_error(who_palettes(palette = "dark"))
 })
